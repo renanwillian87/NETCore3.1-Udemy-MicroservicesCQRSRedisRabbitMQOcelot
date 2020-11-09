e6478c7292ba7b6835a8d91f3b2bf20eb60d3cde
renan santos <renanwillian87@gmail.com>
2020-11-09 09:36:27 -0300

    Add project

---
+++ MicroservicesApp/src/Basket/Basket.API/Data/BasketContext.cs
@@ -0,0 +1,22 @@
+﻿using Basket.API.Data.Interfaces;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Data
+{
+    public class BasketContext : IBasketContext
+    {
+        private readonly ConnectionMultiplexer _redisConnetion;
+
+        public BasketContext(ConnectionMultiplexer redisConnetion)
+        {
+            _redisConnetion = redisConnetion ?? throw new ArgumentNullException(nameof(redisConnetion));
+            Redis = _redisConnetion.GetDatabase();
+        }
+
+        public IDatabase Redis { get; }
+    }
+}
